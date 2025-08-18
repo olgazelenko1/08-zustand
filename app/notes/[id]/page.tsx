4e5160f50d666b317d9c4992bced0fa110056849
OLGA ZELENKO <olichkazelenko@gmail.com>
2025-08-18 21:58:11 +0100

    fix

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -8,11 +8,11 @@ import NoteDetailsClient from './NoteDetails.client';
 
 
 type Props = {
-  params: { id: string };
+   params: Promise<{ id: string }>;
 };
 
 export async function generateMetadata({ params }: Props) {
-  const { id } = params;
+  const { id } = await params;
   const note = await fetchNoteById(id);
   return {
     title: `Note : ${note.title}`,
@@ -34,7 +34,7 @@ export async function generateMetadata({ params }: Props) {
 }
 
 const NoteDetails = async ({ params }: Props) => {
-  const { id } = params;
+  const { id } = await params;
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
